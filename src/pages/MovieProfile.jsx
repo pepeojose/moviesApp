eb7ffb98f4b06bf9b7f1c3633380ef817f8492a1
pepe <sat900@gmx.es>
2022-04-01 18:54:42 +0200

    Crear la vista de los detalles específicos de cada película

--- src/pages/MovieProfile.jsx
+++ src/pages/MovieProfile.jsx
@@ -1,9 +1,29 @@
 import style from './MovieProfile.module.css'
+import movie from './movie.json'
 
 const MovieProfile = () => {
     const API = "https://api.themoviedb.org/3"
+
+
+    const imageUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path
     return (
-        <div>MovieProfile</div>
+        <div className={style.containerDetails} >
+            <article className={style.cardDetails}>
+                <img className={style.cardDetailsImage} src={imageUrl} alt={movie.title} />
+                <div className={style.cardDetailsInfo} >
+                    <h2><span className={style.cardDetailsText}>Título:</span> {movie.title}</h2>
+                    <p><span className={style.cardDetailsText}>Fecha de lanzamiento:</span> {movie.release_date}</p>
+                    <p><span className={style.cardDetailsText}>Duración:</span> {movie.runtime}</p>
+                    <p>
+                        <span className={style.cardDetailsText}>Genero: </span>
+                        {
+                            movie.genres.map(genre => genre.name).join(", ")
+                        }
+                    </p>
+                    <p><span className={style.cardDetailsText}>Descripción:</span> {movie.overview}</p>
+                </div>
+            </article>
+        </div>
     )
 }
 
