fbdf21b0076c377f8908fd6fc7f78dee873bc966
pepe <sat900@gmx.es>
2022-04-08 16:20:58 +0200

    Añadir lógica de las busquedas

--- src/components/MoviesSelection.jsx
+++ src/components/MoviesSelection.jsx
@@ -1,22 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import style from './MoviesSelection.module.css'
 import MovieCard from './MovieCard'
-import Spinner from './Spinner';
+import Spinner from './Spinner'
 import { getData } from "../utils/fetchData"
+import { useQuery } from '../hooks/useQuery'
 
 
 const MoviesSelection = () => {
     const [movies, setMovies] = useState([])
     const [isLoading, setIsLoading] = useState(true)
 
+    const query = useQuery()
+    const search = query.get("search")
 
     useEffect(() => {
         setIsLoading(true)
-        getData("/discover/movie").then(dataMovies => {
+        const searchUrl = search 
+            ? "/search/movie?query=" + search
+            : "/discover/movie"
+        getData(searchUrl).then(dataMovies => {
             setMovies(dataMovies.results)
             setIsLoading(false)
         })
-    }, [])
+    }, [search])
 
     if(isLoading) {
         return <Spinner />
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,41 @@
 import {useState, useEffect} from 'react'
+import { useNavigate } from "react-router-dom"
 import {FaSearch} from "react-icons/fa"
 import style from './Search.module.css'
+import { useQuery } from '../hooks/useQuery'
+
+
 
 
 const Search = () => {
+    const [searchText, setSearchText] = useState("")
+    const navigate = useNavigate()
+
+    const query = useQuery()
+    const search = query.get("search")
+    
+    useEffect(() => {
+        setSearchText(search || "")
+    }, [search])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        navigate("/?search=" + searchText)
+    }
+
+    const handleChange = (e) => {
+        setSearchText(e.target.value)
+    }
+
     return (
-        <form className={style.search}>
+        <form className={style.search} onSubmit={handleSubmit}>
             <div className={style.searchContainer}>
                 <input 
                     type="text" 
                     className={style.searchInput}
                     placeholder="Search Movies"
+                    value={searchText}
+                    onChange={handleChange} 
                 />
                 <button className={style.searchBtn} type="submit">
                     <FaSearch size={20} />
