a2849931dbfd04540432f85903047e0977ab835c
pepe <sat900@gmx.es>
2022-07-11 15:26:24 +0200

    Crear variables de entorno

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,13 +1,13 @@
 import question from "/images/question.png";
 
 export const getData = async (path) => {
-    const API = "https://api.themoviedb.org/3";
+    const API = import.meta.env.VITE_API;
+    const TOKEN = import.meta.env.VITE_TOKEN;
 
     try {
         const res = await fetch(API + path, {
             headers: {
-                Authorization:
-                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzUzN2ZmMTlmMzgxZGQ3YjY3ZWVlMWVhOGI4MTY0YSIsInN1YiI6IjVlM2ExNmU1MGMyNzEwMDAxODc1NTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nOpZ_nBtA93tbzr6-rxD0760tssAAaSppyjRv9anArs",
+                Authorization: "Bearer " + TOKEN,
                 "Content-Type": "application/json;charset=utf-8",
             },
         });
